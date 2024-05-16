ecc3ad94b76638d0ac611eb5c532b480f7c747ef
Sohan Parves <91725648+Rafsanahamedsohan@users.noreply.github.com>
2024-05-16 07:08:42 +0600

    send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -1,6 +1,6 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $to = "your-email@gmail.com"; // Replace with your Gmail address
+    $to = "rafsanahamedsohan@gmail.com"; // Replace with your Gmail address
     $subject = $_POST['subject'];
     $message = $_POST['message'];
     $headers = "From: " . $_POST['email'] . "\r\n";
